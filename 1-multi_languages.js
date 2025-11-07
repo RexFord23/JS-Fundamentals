da04ebd071c5826c88b007fd1d333a7f6a6c9fc0
Rexford Oppong <Rexforddickson@gmail.com>
2025-11-07 18:53:35 +0000

    create 3 lines of JavaScript

---
+++ 1-multi_languages.js
@@ -0,0 +1,12 @@
+// creating first line
+let firstline = "C is fun";
+
+//creating seconf line
+let secondline = "Python is cool";
+
+// creating third line
+let thirdline = "JacaScript is amazing";
+
+console.log(firstline);
+console.log(secondline);
+console.log(thirdline);
