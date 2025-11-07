b221e3731aac7408dd442ffae2988644e9f4bb42
Rexford Oppong <Rexforddickson@gmail.com>
2025-11-07 19:36:25 +0000

    Message of the number of arguments passed

---
+++ 2-arguments.js
@@ -0,0 +1,10 @@
+const arguCount = Process.argv.lenght - 2;
+
+if (arguCount == 0) {
+    console.log("No argument");
+}
+if (arguCount == 1) {
+    console.log("Arguments found");
+} else {
+    console.log("Arguments found");
+}
